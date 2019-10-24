b022a296164b1fc0c920109cd2f7b505b6f82f21
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-10-24 11:09:53 -0700

    update app and index to render new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CurrentTimeFC, CurrentTimeProps } from "./ai-web-similar/CurrentTime";
+import {
+  MouseTrackerFC,
+  MouseTrackerProps
+} from "./ai-web-similar/MouseTracker";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type Point = {
+  x: number;
+  y: number;
+};
+
+type AppState = CurrentTimeProps & MouseTrackerProps;
+
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentTime: new Date(),
+      mousePosition: {
+        x: 0,
+        y: 0
+      }
+    };
+  }
+
+  public componentDidMount(): void {
+    setInterval(() => {
+      this.setState({ currentTime: new Date() });
+    }, 1000);
+  }
+
+  public render() {
+    return (
+      <div className="App" onMouseMove={this.onMouseOver}>
+        <h1>React.FC, Passing the whole state</h1>
+        <span>
+          <CurrentTimeFC {...this.state} />
+        </span>
+        <span>
+          <MouseTrackerFC {...this.state} />
+        </span>
+      </div>
+    );
+  }
+
+  private onMouseOver = (e: React.MouseEvent) => {
+    this.setState({
+      mousePosition: {
+        x: e.screenX,
+        y: e.screenY
+      }
+    });
+  };
 }
-
-export default App;
